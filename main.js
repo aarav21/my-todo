5308ef0df43a665589f03b1211341ee5b296a450
Aarav Rajpal <aaravrajpal21@gmail.com>
2020-11-03 11:30:19 +0530

    test

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function enter(){
+    var userTask = document.getElementById("txtTask").value;
+    console.log(userTask);
+}
+
+
+function task1(){
+    document.getElementById("task1").style.textColor = "green";
+}
